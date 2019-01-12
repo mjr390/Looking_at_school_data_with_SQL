bc7563a5a64976ff89c82737d5e7cbb6ea49da4d
mjr390 <mjr117@yahoo.com>
2019-01-11 22:29:50 -0500

    added comments to explain what is happening and commented out lines that don't need to be run more than once

--- School_Queries.sql
+++ School_Queries.sql
@@ -1,9 +1,6 @@
 use SQL_City_Schools;
-#SELECT COUNT(*) num_Schools
-#FROM schools_data
-#UNION
-#SELECT COUNT(*) num_Students
-#FROM students_data;
+# Change the names of columns containing spaces so they are easier to work with
+# No need to run these lines more than once
+# ALTER TABLE schools_data CHANGE `School ID` `School_ID` VARCHAR(50);
+#ALTER TABLE students_data CHANGE `Student ID` `Student_ID` VARCHAR(50);
 
-ALTER TABLE schools_data CHANGE `School ID` `School_ID` VARCHAR(50);
-ALTER TABLE students_data CHANGE `Student ID` `Student_ID` VARCHAR(50);
