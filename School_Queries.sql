d5438d7eb6f748138d741ee02741e929a0ce43fa
mjr390 <mjr117@yahoo.com>
2019-01-12 13:46:38 -0500

    added a check to see if there are duplicate students

--- School_Queries.sql
+++ School_Queries.sql
@@ -4,3 +4,12 @@ use SQL_City_Schools;
 # ALTER TABLE schools_data CHANGE `School ID` `School_ID` VARCHAR(50);
 #ALTER TABLE students_data CHANGE `Student ID` `Student_ID` VARCHAR(50);
 
+# Check to make sure there are no duplicate students by comparing the number of total students to the number of distinct students
+SELECT COUNT(*)
+FROM(	select DISTINCT Student_ID 
+	FROM students_data) t1;
+    
+SELECT COUNT(*)
+FROM students_data;   
+
+# 
