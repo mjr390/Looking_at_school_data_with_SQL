e69d8863a0cc0eee40bace2e43304c23ffc5209b
mjr390 <mjr117@yahoo.com>
2019-01-11 22:18:26 -0500

    used alter table to change column names with spaces in the to have underscores instead

--- School_Queries.sql
+++ School_Queries.sql
@@ -1,3 +1,9 @@
 use SQL_City_Schools;
-SELECT * FROM students_data
-LIMIT 5;
+#SELECT COUNT(*) num_Schools
+#FROM schools_data
+#UNION
+#SELECT COUNT(*) num_Students
+#FROM students_data;
+
+ALTER TABLE schools_data CHANGE `School ID` `School_ID` VARCHAR(50);
+ALTER TABLE students_data CHANGE `Student ID` `Student_ID` VARCHAR(50);
